8c5a444519dcb88389b466355ea241982ac6bf3f
Shoaib Bin M.Ali <shoaibm.ali14@gmail.com>
2023-12-07 00:25:34 +0500

    changes

--- components/Nav.js
+++ components/Nav.js
@@ -26,8 +26,39 @@ export const navData = [
   },
 ];
 
+
+// Next Link
+import Link from 'next/link';
+
+// Next Router
+import { useRouter } from 'next/router';
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+    <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+      {/* inner */}
+      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+        {navData.map((navData, index) => {
+          return <Link className={`${navData.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300`} key={index} href={navData.path}>
+            {/* toptip */}
+            <div className='absolute pr-14 right-0 hidden xl:group-hover:flex rounded-[3px]'>
+              <div className='bg-white relative flex text-primary items-center p-[6px]'>
+                <div className='text-[12px] leading-none font-semibold capitalize'>
+                  {navData.name}
+                  </div>
+                  {/* Triangle */}
+                  <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+              </div>
+            </div>
+            {/* icons */}
+            <div>{navData.icon}</div>
+          </Link>
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
